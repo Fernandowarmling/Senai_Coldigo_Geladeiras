f5c8dd3363c51d4106e062b5b4d9634aeb7d9d2b
Fernandowarmling <warmlingfernando@gmail.com>
2023-06-26 20:55:50 -0300

    Change: Finalização da ot 10

--- Senai_ProjetoTilhaWeb/exemploServlet/src/main/java/br/com/gcontato/servlet/AdicionaContatoServlet.java
+++ Senai_ProjetoTilhaWeb/exemploServlet/src/main/java/br/com/gcontato/servlet/AdicionaContatoServlet.java
@@ -1,4 +1,5 @@
 package br.com.gcontato.servlet;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -6,34 +7,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class AdicionaContatoServlet extends HttpServlet{
-	
+public class AdicionaContatoServlet extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
+
 	public AdicionaContatoServlet() {
 		super();
 	}
-	
+
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
-	throws ServletException, IOException{
+			throws ServletException, IOException {
+
 		
-	PrintWriter out = response.getWriter();
-	String nome = request.getParameter("name");
-	out.println(nome);
-	
+		PrintWriter out = response.getWriter();
+		String nome = request.getParameter("nome");
+		out.println(nome);
+
+		String endereco = request.getParameter("endereco");
+		out.println(endereco);
+
+		String telefone = request.getParameter("fone");
+		out.println(telefone);
+
 	}
-	
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException{
+			throws ServletException, IOException {
 		
-			}
-	
+		
+
+	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException{
+			throws ServletException, IOException {
 		
-			}
-	
-	
-	
+		
+		
+
+	}
 
 }
